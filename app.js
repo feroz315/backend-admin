aafb612415853852796502e301304bf138b208a0
FEROZ KHAN <Feroz3151@gmail.com>
2024-03-18 23:38:26 +0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 import mongoose from 'mongoose';
-import { products} from './data.js'
+// import { products} from './data.js'
 import express from "express";
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const db = "mongodb+srv://admin:admin@crudapp.ncgm6eu.mongodb.net/"
+const db = "mongodb+srv://admin:admin@crudapp.ncgm6eu.mongodb.net/";
 
 
 mongoose.connect(db)
 mongoose.connection.on("connected", () => console.log("mongose connect"));
 mongoose.connection.on("error", (err) => console.log("error mongo",err));
-
-// .then((res) => console.log("mongo db connect"))
-// .catch((error)=> console.log("err",error))
 
 
 app.use(express.json());
@@ -23,25 +20,10 @@ app.use(express.urlencoded({ extended : true}));
 
 
 app.get("/",((req,res)=> {
-        res.send("server on")
+        res.json({
+            message: "Server on hai"
+        })
 }))
-
-// app.get("/allproduct", ((req,res) => {
-//     console.log("respone", req.query)
-//     if(req.query.id){
-//     const filterdata = products.filter((product) => 
-//             product.id == req.query.id
-//         );
-//         res.send(filterdata)
-//         return;
-//     }
-//     res.send(products);
-// }));
-
-// app.get("/product/:id",((req,res)=> {
-//         console.log(req.params.id,"params");
-//         res.send("single product")
-// }))
 
 app.post("/createuser",((req,res) => {
     console.log("user",req.body)
