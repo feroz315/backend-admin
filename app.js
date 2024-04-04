9022d066d8b3e0c87e403401606583903c23f1a9
FEROZ KHAN <Feroz3151@gmail.com>
2024-04-05 00:02:49 +0500

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import routes from './Routes/Routes.js';
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DATABASE_URL;
 console.log(DB_URL);
 
