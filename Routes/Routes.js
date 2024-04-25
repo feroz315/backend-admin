30513dff77672549c79e4d1d977ac8bf9ffe5117
FEROZ KHAN <Feroz3151@gmail.com>
2024-04-25 12:28:13 +0500

    authmiddleware

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,10 +2,11 @@ import express from "express";
 const routes = express.Router();
 import { getAllProducts,getSingleProduct } from "../Controller/Products.js";
 import { UserSignup,UserLogin } from "../Controller/Auth.js";
+import { authMiddleware } from "../Middlewares/index.js";
 
 
 
-routes.get("/products", getAllProducts);
+routes.get("/products", getAllProducts,authMiddleware);
 routes.get("/products/:id", getSingleProduct);
 routes.post("/signup", UserSignup);
 routes.post("/login", UserLogin);
